e0dd8b8a0e710f9a9d584b6afe4361760f4e3b02
s_sasaki19861217 <the.funky4.scoobie.do@gmail.com>
2018-09-04 19:14:58 +0900

    2018/09/04作業分

--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@
 $(function() {
   $('.di-input__heart').each(function() {
     for (var i = 0; i < 5; i ++) {
-      $(this).append('<a>');
+      $(this).append('<span>');
     }
   });
-  $('.di-input__heart>a').on('click', function() {
+  $('.di-input__heart > span').on('click', function() {
     var index = $(this).index();
     $(this).siblings().removeClass('on');
     for (var i = 0; i < index; i++) {
-      $(this).parent().find('a').eq(i).addClass('on');
+      $(this).parent().find('span').eq(i).addClass('on');
     }
     $(this).parent().find('.di-input__heart').attr('value', index);
   });
 });
 
 //モーダル
-function jsDialog(dialog){
+function jsDialog(dialog,title,contents){
   $(dialog).removeClass('hide');
   $(this).blur();
   if($("#js-dialog__overlay")[0]) return false;
@@ -27,7 +27,8 @@ function jsDialog(dialog){
 
   centeringModalSyncer() ;
   $(".js-dialog__content").fadeIn("slow");
-
+  $(".js-dialog__content h4").text(title);
+  $(".js-dialog__inner").html(contents);
   $("#js-dialog__overlay,.js-dialog__close").unbind().click(function(){
     $(".js-dialog__hide").addClass('hide');
     $(".js-dialog__content,#js-dialog__overlay").fadeOut("slow" , function(){
@@ -38,7 +39,7 @@ function jsDialog(dialog){
 
 //削除ボタンを押したらダイアログを表示
 $(".js-dialog__open").click(function(){
-  jsDialog("#js-dialog__del");
+  jsDialog("#js-dialog__area", "本当に削除する？", '<div class="di-btn__space"><a class="di-btn di-btn__defa" href="t.cgi?t=m/diary/list&<!--DEF_P-->">削除する</a></div><div class="di-btn__space"><button class="di-btn di-btn__del js-dialog__close">削除しない</button></div>');
 });
 
 
@@ -50,8 +51,8 @@ function centeringModalSyncer() {
   var w = $( window ).width() ;
   var h = $( window ).height() ;
 
-  //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-  //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+  //    var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+  //    var ch = $( "#modal-content" ).outerHeight( {margin:true} );
   var cw = $( ".js-dialog__content" ).outerWidth();
   var ch = $( ".js-dialog__content" ).outerHeight();
 
